49dd02b054400095268d4dd83c0f4cffcdce1ca3
LaunMatt <launaymatthew15@gmail.com>
2025-03-26 20:20:37 +0100

    Phase 3 : tests unitaires et fonctionnels

---
+++ Formes Phase 2/FormesTests/modele/CarreTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Formes.modele;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formes.modele.Tests
+{
+    [TestClass()]
+    public class CarreTests
+    {
+        [TestMethod()]
+        public void CarreTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void PerimetreTest()
+        {
+            Carre c = new Carre(5);
+            double perimetre = c.Perimetre();
+            Assert.AreEqual(20, perimetre);
+        }
+
+        [TestMethod()]
+        public void AireTest()
+        {
+
+        }
+    }
+}
